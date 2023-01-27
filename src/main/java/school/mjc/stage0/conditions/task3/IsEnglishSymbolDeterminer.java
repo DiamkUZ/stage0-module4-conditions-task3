9b8516c7d074eb687ad7fe4aa2aa982af062861a
DiamkUZ <diamkms@yandex.ru>
2023-01-27 11:42:59 +0300

    All done

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,17 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-        if ((int) symbol >= 65 && (int) symbol <= 122) {
+
+        String englishAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+        if (englishAlphabet.contains("" + symbol)) {
             System.out.println("English");
         } else System.out.println("Non English");
 
-        System.out.println((int) symbol);
-
-    }
-
-    public static void main(String[] args) {
-        IsEnglishSymbolDeterminer englishDeterminer = new IsEnglishSymbolDeterminer();
-
-        englishDeterminer.isEnglishSymbol('A');
     }
 }
